532de47a5e89ba8721bacfb4c828c4142a4e9b1b
Serge Basangovs <sergejs.basangovs@gmail.com>
2023-11-05 15:32:27 +0200

    feat: custom hook use local storage

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react'
+
+import { isServer } from '@/utils/common'
+
+type HookReturnValue<D> = [D | null, (value: D) => void]
+
+const getValueFromLocalStorage = <P>(key: string, initialValue: P): P => {
+  if (isServer()) {
+    return initialValue
+  }
+  try {
+    const item = window.localStorage.getItem(key)
+    return item ? JSON.parse(item) : initialValue
+  } catch (error) {
+    console.log(error)
+    return initialValue
+  }
+}
+
+export const useLocalStorage = <T>(
+  key: string,
+  initialValue: T,
+): HookReturnValue<T> => {
+  const [storedValue, setStoredValue] = useState<T | null>(null)
+
+  const setValue = (value: T) => {
+    if (isServer()) return
+
+    try {
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value
+      setStoredValue(valueToStore)
+
+      window.localStorage.setItem(key, JSON.stringify(valueToStore))
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    const value = getValueFromLocalStorage(key, initialValue)
+    setStoredValue(value)
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return [storedValue, setValue]
+}
